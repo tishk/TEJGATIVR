3313de8332facb4ed28c9d17a2fe9905ea987388
tishk <tishk.oram@gmail.com>
2017-07-25 12:30:28 +0430

    second commit

--- TestPin/src/main.java
+++ TestPin/src/main.java
@@ -21,7 +21,7 @@ public class main {
     public static void main(String[] arg) throws IOException, ServerNotActiveException {
 
 //        AuthenticatePin2 authenticatePin2=new AuthenticatePin2();
-                    testPin1();
+//                    testPin1();
 //            testTransactionarzi();
     }
     private static void testTransactionarzi() throws IOException, ServerNotActiveException {
